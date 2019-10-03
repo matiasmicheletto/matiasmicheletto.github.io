2e80fd9fd4236218e2d37b24e8c57f915193eebf
Matias Micheletto <matiasmicheletto@gmail.com>
2019-10-02 23:55:38 -0300

    Primer commit

---
+++ js/index.js
@@ -0,0 +1,131 @@
+$(function () {
+    
+    "use strict";
+    
+    var win = $(window),
+        htmlBody = $("html, body"),
+        scrollToTop = $(".scroll-top"),
+        progressCheck = false,
+        factsCheck = false;
+        
+    $('.preloader').delay(200).fadeOut(700, function () {
+        $(this).remove();
+    });
+    
+    $(".control-bar .menu-toggle").on("click", function () {
+        $(this).toggleClass("active");
+        $(".slide-menu").toggleClass("active");
+    });
+    
+    $(".slide-menu .menu-list li a").on("click", function (e) {
+        e.preventDefault();
+        htmlBody.animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 600);
+    });
+    
+    $("section").each(function () {
+        if (win.scrollTop() >= $(this).offset().top - 1) {
+            $(".slide-menu .menu-list li a[href='#" + $(this).attr("id") + "']").addClass("active").parent().siblings().find("a").removeClass("active");
+        }
+    });
+    win.on("scroll", function () {
+        $("section").each(function () {
+            if (win.scrollTop() >= $(this).offset().top - 1) {
+                $(".slide-menu .menu-list li a[href='#" + $(this).attr("id") + "']").addClass("active").parent().siblings().find("a").removeClass("active");
+            }
+        });
+    });
+    
+    function skillsPogress() {
+        $('.chart').easyPieChart({
+            size: 140,
+            barColor: '#dca52e',
+            trackColor: '#313131',
+            scaleColor: false,
+            lineWidth: 2,
+            scaleLength: 4,
+            lineCap: 'circle',
+            animate: {
+                duration: 2000,
+                enabled: true
+            }
+        });
+    }
+    
+    if (!progressCheck && $(this).scrollTop() >= $(".skills").offset().top - 300) {
+        skillsPogress();
+        progressCheck = true;
+    }
+    
+    win.on("scroll", function () {
+        
+        if (!progressCheck && $(this).scrollTop() >= $(".skills").offset().top - 300) {
+            skillsPogress();
+            progressCheck = true;
+        }
+        
+    });
+    
+    $("#control li").on('click', function () {
+        $(this).addClass('active').siblings("li").removeClass('active');
+    });
+    $('#filtr-container').filterizr({
+        animationDuration: 0.4
+    });
+    
+    if ($('.portfolio-content .item')[0]) {
+        $('.portfolio-content .item').magnificPopup({
+            delegate: '.icon-img',
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
+        });
+    }
+    
+    if (!factsCheck && $(this).scrollTop() >= $(".facts").offset().top - 400) {
+        $(".facts .fact-number").countTo();
+        factsCheck = true;
+    }
+    
+    win.on("scroll", function () {
+        if (!factsCheck && $(this).scrollTop() >= $(".facts").offset().top - 400) {
+            $(".facts .fact-number").countTo();
+            factsCheck = true;
+        }
+    });
+    
+    $(".testimonials .owl-carousel").owlCarousel({
+        items: 1,
+        autoplay: true,
+        smartSpeed: 500,
+        margin: 10,
+        loop: true,
+        autoplayHoverPause: true,
+        responsiveClass: true
+    });
+    
+    function scrollUp() {
+        if (win.scrollTop() >= 1200) {
+            scrollToTop.addClass("active");
+        } else {
+            scrollToTop.removeClass("active");
+        }
+    }
+    
+    scrollUp();
+    
+    win.on("scroll", function () {
+        scrollUp();
+    });
+    
+    scrollToTop.on("click", function (e) {
+        e.preventDefault();
+        htmlBody.animate({
+            scrollTop: 0
+        }, 800);
+    });
+    
+    new WOW().init();
+});
